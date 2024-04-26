f669972c110663bcfce7f8e4d2f52e141ea8fdc6
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2024-04-26 12:02:21 +0600

    added decentralized casino contract

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -2,19 +2,31 @@
 pragma solidity 0.8.24;
 import "hardhat/console.sol";
 
-contract Casino {
+import {VRFV2WrapperConsumerBase } from "@chainlink/contracts/src/v0.8/vrf/VRFV2WrapperConsumerBase.sol";
+
+contract Casino is VRFV2WrapperConsumerBase {
     mapping(address => uint256) public gameEthValues;
     mapping(address => uint256) public blockNumbersToBeUsedMapping;
+    address LINK_Token = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+    address wrapper = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
+
+constructor()VRFV2WrapperConsumerBase(LINK_Token,wrapper){
+
+}
+    function fundBank() payable external {
+
+    }
 
     function playGame() external payable {
-        uint256 blockNumberToBeUsed = blockNumbersToBeUsedMapping[msg.sender];
+        console.log("Block Number", block.number);
+        uint256 blockNumberToBeUsed = blockNumbersToBeUsedMapping[msg.sender]; //  blockNumbersToBeUsedMapping[address];
         console.log("blockNumberToBeUsed", blockNumberToBeUsed);
         if (blockNumberToBeUsed == 0) {
             // first run, determine block number to be used
-            console.log("Block Number", block.number);
-            blockNumbersToBeUsedMapping[msg.sender] = block.number + 2;
+            console.log("Block Number, WHEN blockNumberToBeUsed == 0", block.number);
+            blockNumbersToBeUsedMapping[msg.sender] = block.number + 128;
             gameEthValues[msg.sender] = msg.value;
-            console.log("gameEthValues[msg.sender]", gameEthValues[msg.sender]);
+            console.log("gameEthValues[msg.sender], WHEN blockNumberToBeUsed == 0", gameEthValues[msg.sender]);
             return;
         }
 
@@ -27,5 +39,7 @@ contract Casino {
             (bool sucess, ) = msg.sender.call{value: winningAmount}("");
             require(sucess, "Transfer Failed");
         }
+        
+       // console.log("ENDING balance", );
     }
 }
